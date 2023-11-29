2649c8b2be3940a268066d678b6117075e70dda1
Maxwell Dubin <maxdhs@gmail.com>
2023-11-29 13:44:07 -0500

    ...

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,47 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  const [isGameActive, setIsGameActive] = useState(true);
+  const [boxes, setBoxes] = useState([
+    { id: 1, mark: "" },
+    { id: 2, mark: "" },
+    { id: 3, mark: "" },
+    { id: 4, mark: "" },
+    { id: 5, mark: "" },
+    { id: 6, mark: "" },
+    { id: 7, mark: "" },
+    { id: 8, mark: "" },
+    { id: 9, mark: "" },
+  ]);
+  const [isPlayer1Turn, setIsPlayer1Turn] = useState(true);
+
+  function handleClick(box) {
+    // if it's player 1 turn update box object with x
+    // i need to update box state
+    // i want all the old boxes minus the box we clicked on
+    const oldBoxes = boxes.filter((_box) => _box.id !== box.id);
+    const updatedBox = { ...box };
+    if (isPlayer1Turn) {
+      updatedBox.mark = "X";
+    } else {
+      updatedBox.mark = "O";
+    }
+    setBoxes([...oldBoxes, updatedBox]);
+    setIsPlayer1Turn(!isPlayer1Turn);
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div id="board">
+      {boxes
+        .sort((a, b) => (a.id > b.id ? 1 : -1))
+        .map((box) => {
+          return (
+            <div onClick={() => handleClick(box)} className="box" key={box.id}>
+              {box.mark}
+            </div>
+          );
+        })}
+    </div>
+  );
 }
